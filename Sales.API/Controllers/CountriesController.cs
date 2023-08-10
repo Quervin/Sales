c93134197962bf7a68fb84e400df7dbf30faadb2
querv <quervinhidalgo@gmail.com>
2023-08-09 20:48:01 -0600

    Complete countries Api COntroller

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -20,6 +20,17 @@ namespace Sales.API.Controllers
         public async Task<IActionResult> GetAsync()
         {
             return Ok(await _context.Countries.ToListAsync());
+        }
+        
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if(country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
         } 
 
         [HttpPost]
@@ -28,6 +39,28 @@ namespace Sales.API.Controllers
             _context.Add(country);
             await _context.SaveChangesAsync();
             return Ok(country);
+        }
+        
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
 
     }
