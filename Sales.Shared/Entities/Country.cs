c447099a5c6ab65dbedba2f45400567340ef6b0d
querv <quervinhidalgo@gmail.com>
2023-08-18 21:03:07 -0600

    Add states stasNumber

--- Sales.Shared/Entities/Country.cs
+++ Sales.Shared/Entities/Country.cs
@@ -10,5 +10,9 @@ namespace Sales.Shared.Entities
         [Required(ErrorMessage = "El campo {0} es obligatortio.")]
         [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres")]
         public string Name { get; set; } = null!;
+
+        public ICollection<State>? States { get; set; }
+
+        public int StatesNumber => States == null ? 0 : States.Count;
     }
 }
