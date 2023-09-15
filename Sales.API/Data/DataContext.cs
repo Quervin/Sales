b2d521c165875aae3d5d0f100683c0a323773be1
querv <quervinhidalgo@gmail.com>
2023-09-14 21:55:33 -0600

    Finishing Crud Category

--- Sales.API/Data/DataContext.cs
+++ Sales.API/Data/DataContext.cs
@@ -7,6 +7,7 @@ namespace Sales.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
+        public DbSet<Category> Categories { get; set; }
         public DbSet<City> Cities { get; set; }
 
         public DbSet<Country> Countries { get; set; }
@@ -16,6 +17,7 @@ namespace Sales.API.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex("CountryId", "Name").IsUnique();
             modelBuilder.Entity<City>().HasIndex("StateId", "Name").IsUnique();
