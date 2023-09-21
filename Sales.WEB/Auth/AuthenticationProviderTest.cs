427da8803b7e97880f352ff466b857946ed0a88c
querv <quervinhidalgo@gmail.com>
2023-09-20 21:07:52 -0600

    Security hardcoded on Web

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var kerUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Ker"),
+                new Claim("LastName", "Moya"),
+                new Claim(ClaimTypes.Name, "zulu@yopmail.com"), 
+                new Claim(ClaimTypes.Role, "User")
+            },
+            authenticationType: "test");
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(kerUser)));
+        }
+
+    }
+}
